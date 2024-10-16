6daa40a4dc1623dd44aba32f4c3d3ec19fffed3d
deviank <tensaidevi@gmail.com>
2024-10-16 18:23:20 +0800

    Minor fix
    Re-referenced objects in R&D Performance Measure tab

--- Assets/Scripts/RD/PerformanceMeasureRD.cs
+++ Assets/Scripts/RD/PerformanceMeasureRD.cs
@@ -79,7 +79,7 @@ namespace RL.RD
             float recall = arResults.CalculateRecall();
             float fScore = arResults.CalculateFScore();
 
-            arMetricsTmp.text += @$"Precision: {precision}
+            arMetricsTmp.text = @$"Precision: {precision}
 Recall: {recall}
 F-Score: {fScore}";
         }
@@ -123,7 +123,7 @@ F-Score: {fScore}";
             float recall = gnbResults.CalculateRecall();
             float fScore = gnbResults.CalculateFScore();
 
-            gnbMetricsTmp.text += @$"Precision: {precision}
+            gnbMetricsTmp.text = @$"Precision: {precision}
 Recall: {recall}
 F-Score: {fScore}";
         }
