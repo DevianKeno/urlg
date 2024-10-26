f0d0b0674a765626688d7a20b7bb82e2150b68df
Goyo <edrickdevilla.me@gmail.com>
2024-10-26 21:33:23 +0800

    Save, Calculate, Export

--- Assets/Scripts/RD/PerformanceMeasureRD.cs
+++ Assets/Scripts/RD/PerformanceMeasureRD.cs
@@ -10,6 +10,7 @@ using Newtonsoft.Json;
 using RL.Classifiers;
 using System;
 using System.Linq;
+using System.Text;
 
 namespace RL.RD
 {
@@ -25,6 +26,9 @@ namespace RL.RD
         [SerializeField] TextMeshProUGUI gnbInfoTmp;
         [SerializeField] TextMeshProUGUI arMetricsTmp;
         [SerializeField] TextMeshProUGUI gnbMetricsTmp;
+        [SerializeField] TextMeshProUGUI dataTmp;
+        [SerializeField] TextMeshProUGUI meanTmp;
+
 
         [Header("Buttons")]
         [SerializeField] Button arResultsBtn;
@@ -33,6 +37,24 @@ namespace RL.RD
         [SerializeField] Button gnbFolderBtn;
         [SerializeField] Button calculateARBtn;
         [SerializeField] Button calculateGNBBtn;
+        [SerializeField] Button saveARBtn;
+        [SerializeField] Button saveGNBBtn;
+        [SerializeField] Button calculateMeanBtn;
+        [SerializeField] Button exportBtn;
+
+        [Header("Other")]
+        [SerializeField] Transform dataContainer;
+        [SerializeField] GameObject dataEntryPrefab;
+
+        private int playerIdCounter = 1;
+
+        private List<float> arPrecisions = new List<float>();
+        private List<float> arRecalls = new List<float>();
+        private List<float> arFScores = new List<float>();
+
+        private List<float> gnbPrecisions = new List<float>();
+        private List<float> gnbRecalls = new List<float>();
+        private List<float> gnbFScores = new List<float>();
 
         void Awake()
         {
@@ -44,6 +66,12 @@ namespace RL.RD
 
             calculateARBtn.onClick.AddListener(CalculateAR);
             calculateGNBBtn.onClick.AddListener(CalculateGNB);
+
+            saveARBtn.onClick.AddListener(SaveARMetrics);
+            saveGNBBtn.onClick.AddListener(SaveGNBMetrics);
+
+            calculateMeanBtn.onClick.AddListener(CalculateMean);
+            exportBtn.onClick.AddListener(ExportToCSV);
         }
 
         void Start()
@@ -115,6 +143,26 @@ namespace RL.RD
             arMetricsTmp.text = @$"Precision: {precision}
 Recall: {recall}
 F-Score: {fScore}";
+        }
+        void SaveARMetrics()
+        {
+            string playerId = $"Player {playerIdCounter}";
+
+            float precision = arResults.CalculatePrecision();
+            float recall = arResults.CalculateRecall();
+            float fScore = arResults.CalculateFScore();
+
+            arPrecisions.Add(precision);
+            arRecalls.Add(recall);
+            arFScores.Add(fScore);
+
+            string displayText = $"{playerId}      Accept/Reject     {precision:F6}     {recall:F6}     {fScore:F6}";
+
+            GameObject newEntry = Instantiate(dataEntryPrefab, dataContainer);
+            TextMeshProUGUI dataTMP = newEntry.GetComponentInChildren<TextMeshProUGUI>();
+
+            dataTMP.text = displayText;
+            playerIdCounter++;
         }
 
         #endregion
@@ -188,6 +236,41 @@ Recall: {recall}
 F-Score: {fScore}";
         }
 
+        void SaveGNBMetrics()
+        {
+            string playerId = $"Player {playerIdCounter}";
+
+            float precision = gnbResults.CalculatePrecision();
+            float recall = gnbResults.CalculateRecall();
+            float fScore = gnbResults.CalculateFScore();
+
+            gnbPrecisions.Add(precision);
+            gnbRecalls.Add(recall);
+            gnbFScores.Add(fScore);
+
+            string displayText = @$"{playerId}            GNB             {precision:F6}     {recall:F6}     {fScore:F6}";
+
+            GameObject newEntry = Instantiate(dataEntryPrefab, dataContainer);
+            TextMeshProUGUI dataTMP = newEntry.GetComponentInChildren<TextMeshProUGUI>();
+
+            dataTMP.text = displayText;
+            playerIdCounter++;
+        }
+
+        void CalculateMean()
+        {
+            float arMeanPrecision = arPrecisions.Count > 0 ? arPrecisions.Average() : 0;
+            float arMeanRecall = arRecalls.Count > 0 ? arRecalls.Average() : 0;
+            float arMeanFScore = arFScores.Count > 0 ? arFScores.Average() : 0;
+
+            float gnbMeanPrecision = gnbPrecisions.Count > 0 ? gnbPrecisions.Average() : 0;
+            float gnbMeanRecall = gnbRecalls.Count > 0 ? gnbRecalls.Average() : 0;
+            float gnbMeanFScore = gnbFScores.Count > 0 ? gnbFScores.Average() : 0;
+
+            meanTmp.text = @$"   {arMeanPrecision:F6}     {arMeanRecall:F6}     {arMeanFScore:F6}
+   {gnbMeanPrecision:F6}     {gnbMeanRecall:F6}     {gnbMeanFScore:F6}";
+        }
+
         #endregion
 
 
@@ -238,5 +321,30 @@ FN: {gnbResults.FNCount}
 Recall:
 F-score:";
         }
+
+        public void ExportToCSV()
+        {
+            StringBuilder csvContent = new StringBuilder();
+            csvContent.AppendLine("Player ID,Algorithm,Precision,Recall,F-Score");
+
+            // Add saved Accept/Reject metrics
+            for (int i = 0; i < arPrecisions.Count; i++)
+            {
+                csvContent.AppendLine($"{i + 1},Accept/Reject,{arPrecisions[i]:F6},{arRecalls[i]:F6},{arFScores[i]:F6}");
+            }
+
+            // Add saved GNB metrics
+            for (int i = 0; i < gnbPrecisions.Count; i++)
+            {
+                csvContent.AppendLine($"{i + 1},GNB,{gnbPrecisions[i]:F6},{gnbRecalls[i]:F6},{gnbFScores[i]:F6}");
+            }
+
+            // Define file path and save the file
+            string filePath = Path.Combine(Application.persistentDataPath, "PerformanceMetrics.csv");
+            File.WriteAllText(filePath, csvContent.ToString());
+
+            Debug.Log($"Exported to {filePath}");
+        }
+
     }
 }
